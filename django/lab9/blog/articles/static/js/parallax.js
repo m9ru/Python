02043bc049380cc08510d8fde8e508a369b7021f
m9ru <m9ru@users.noreply.github.com>
2019-06-08 23:24:30 +0300

    last comm

--- django/lab9/blog/articles/static/js/parallax.js
+++ django/lab9/blog/articles/static/js/parallax.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
-    var lotusFirstPosition=$('.icons-for-parallax').offset().top; 
-    var currentPosition; 
-    $(document).scroll(function(){ 
-        if($('.icons-for-parallax').offset().top<=$(document).scrollTop()){ 
-            currentPosition=$(document).scrollTop(); 
-            $('.icons-for-parallax').css('top',$(document).scrollTop()+'px'); 
-            $('.icons-for-parallax').css({ 
-            'position':'absolute' 
-            }) 
-        } 
-        else if(currentPosition>=lotusFirstPosition){ 
-            currentPosition=$(document).scrollTop(); 
-            $('.icons-for-parallax').css('top',$(document).scrollTop()+'px'); 
-        } 
-        else{ 
-            $('.icons-for-parallax').css('top',lotusFirstPosition+'px'); 
-        } 
+    var yPosition;
+    var scrolled = 0;
+    var $parallaxElements = $('.icons-for-parallax img');
+    var $parallaxElement = $('.image');
+    $(window).scroll(function() { 
+    	scrolled = $(window).scrollTop(); 
+    	for (var i = 0; i < $parallaxElements.length; i++){ 
+    		yPosition = (scrolled * 0.15*(i + 1)); 
+    		$parallaxElements.eq(2-i).css({ top: yPosition }); 
+    	}
+    	yPosition = (scrolled * 0.25);
+    	$parallaxElement.eq(0).css({ top: yPosition });
     }); 
 });
